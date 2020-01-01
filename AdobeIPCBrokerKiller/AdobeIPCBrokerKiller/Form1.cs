314ea5b4c4b9cedde83babc1f139cccbb0fb6561
Wyatt <wyatth120@gmail.com>
2020-01-01 08:54:25 -0500

    Add files via upload

---
+++ AdobeIPCBrokerKiller/AdobeIPCBrokerKiller/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdobeIPCBrokerKiller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcessesByName("AdobeIPCBroker"))
+            {
+                try
+                {
+                    p.Kill();
+                    p.WaitForExit(); 
+                }
+                catch (Win32Exception winException)
+                {
+                   
+                }
+                catch (InvalidOperationException invalidException)
+                {
+                   
+                }
+            }
+        }
+    }
+}
